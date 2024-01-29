05a75c74480318a2e0037e6a946af361b3eb3462
DamianRoc13 <146010485+DamianRoc13@users.noreply.github.com>
2024-01-29 17:21:34 -0500

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
+import { BoodyComponent } from './boody/boody.component';
+import { HeaderComponent } from './header/header.component';
+import { InformationComponent } from './information/information.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BannerComponent, HeaderComponent, BoodyComponent, InformationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/boody/boody.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boody',
+  standalone: true,
+  imports: [],
+  templateUrl: './boody.component.html',
+  styleUrl: './boody.component.css'
+})
+export class BoodyComponent {
+
+}
